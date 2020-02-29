272b963729ace09109c8ceaacf806e697bc1f0ec
LexWong <914088735@qq.com>
2020-02-29 15:38:18 +0800

    improve(ots): improve the module.

--- examples/basic-example/outputs.tf
+++ examples/basic-example/outputs.tf
@@ -1,25 +1,24 @@
 output "this_instance_id" {
   description = "The id of the ots instance"
-  value       = local.instance_name
+  value       = module.ots.this_instance_id
 }
 
 output "this_instance_name" {
   description = "The name of the ots instance"
-  value       = local.instance_name
+  value       = module.ots.this_instance_name
 }
 
 output "this_vpc_name" {
   description = "The vpc name of the ots instance attached."
-  value       = concat(alicloud_ots_instance_attachment.this.*.vpc_name, [""])[0]
+  value       = module.ots.this_instance_name
 }
 
 output "this_vswitch_id" {
   description = "The vswitch id of the ots instance attached."
-  value       = concat(alicloud_ots_instance_attachment.this.*.vswitch_id, [""])[0]
+  value       = module.ots.this_instance_name
 }
 
 output "this_table_name" {
   description = "The name of the ots table"
-  value       = concat(alicloud_ots_table.this.*.table_name, [""])[0]
+  value       = module.ots.this_instance_name
 }
-
--- modules/instance/main.tf
+++
@@ -1,7 +0,0 @@
-resource "alicloud_ots_instance" "this" {
-  name          = "${var.name}"
-  description   = "${var.description}"
-  accessed_by   = "${var.accessed_by}"
-  instance_type = "${var.instance_type}"
-  tags          = "${var.tags}"
-}
