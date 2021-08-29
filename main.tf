53c38cf5d0160c4e13b6e9a2e86d13770fdad63e
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-08-29 17:37:20 +0800

    Removes the provider setting and improves the readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/table-store"
-}
-
 locals {
   instance_name = var.use_existing_instance ? var.existing_ots_instance_name : concat(alicloud_ots_instance.this.*.name, [""])[0]
 }
