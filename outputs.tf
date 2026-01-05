eba8e91d182bc843c4df6e6e44beda5b8ed70c1c
shanye <shanye.xzq@alibaba-inc.com>
2026-01-05 19:59:45 +0800

    feat!: remove unused variables

--- outputs.tf
+++ outputs.tf
@@ -10,16 +10,16 @@ output "this_instance_name" {
 
 output "this_vpc_name" {
   description = "The vpc name of the ots instance attached."
-  value       = concat(alicloud_ots_instance_attachment.this.*.vpc_name, [""])[0]
+  value       = concat(alicloud_ots_instance_attachment.this[*].vpc_name, [""])[0]
 }
 
 output "this_vswitch_id" {
   description = "The vswitch id of the ots instance attached."
-  value       = concat(alicloud_ots_instance_attachment.this.*.vswitch_id, [""])[0]
+  value       = concat(alicloud_ots_instance_attachment.this[*].vswitch_id, [""])[0]
 }
 
 output "this_table_name" {
   description = "The name of the ots table"
-  value       = concat(alicloud_ots_table.this.*.table_name, [""])[0]
+  value       = concat(alicloud_ots_table.this[*].table_name, [""])[0]
 }
 
